f56f0ad0912be67b022a77b4c26c91f669567551
Henrik <htlemke@gmail.com>
2014-04-06 12:14:48 -0700

    some stuff

---
+++ lsfHelper.py
@@ -0,0 +1,94 @@
+import sys
+import datetime
+import subprocess
+import os
+preamble  = 'import sys\nimport ixppy\nfrom ixppy.lsfHelper import import_file\n'
+queue = 'psnehq'
+
+def import_file(full_path_to_module,glb):
+  #try:
+        import os
+        module_dir, module_file = os.path.split(full_path_to_module)
+        module_name, module_ext = os.path.splitext(module_file)
+        save_cwd = os.getcwd()
+        os.chdir(module_dir)
+	os.system('pwd')
+	print module_name
+	module_obj = __import__(module_name)
+	#exec('import '+module_name)
+	module_obj.__file__ = full_path_to_module
+        glb[module_name] = module_obj
+	#raise NotImplementedError('Use the source, luke!')
+        os.chdir(save_cwd)
+	return module_name
+    #except Exception,e:
+	#print e
+	#raise ImportError
+
+def get_module_name(full_path_to_module):
+    try:
+        import os
+        module_dir, module_file = os.path.split(full_path_to_module)
+        module_name, module_ext = os.path.splitext(module_file)
+	return module_name
+    except Exception,e:
+        print e
+        raise ImportError
+
+def writePyFile(funcs,experiment=None,save=True):
+  timestr = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+  filename = 'lsf_autocall_'+timestr+'.py'
+  filestr = preamble
+  if experiment==None:
+    filestr += 'experiment = sys.argv[1]\nprint "got experiment %s"%experiment\n'
+    filestr += 'runno = int(sys.argv[2])\nprint "got run %d"%runno\n'
+  else:
+    filestr += 'runno = int(sys.argv[1])\nprint "got run %d"%runno\n'
+  modnames = []
+  for func in funcs:
+    modFina = sys.modules[func.__module__].__file__
+    modFina = os.path.abspath(modFina)
+    modnames.append(get_module_name(modFina))
+    filestr += 'import_file(\'%s\',globals())\n'%modFina
+  if experiment==None:
+    filestr += 'd = eval(\'ixppy.dataset((\\\'%s\\\',%d))\'%(experiment,runno))\n'
+  else:
+    filestr += 'd = ixppy.dataset((\'%s\',runno))\n'%experiment
+  for func,modname in zip(funcs,modnames):
+    funcname = func.__name__
+    filestr += modname+'.'+funcname+'(d)\n'
+    if save:
+      filestr += 'd.save(force=True)\n'
+  h = file(filename,'w')
+  h.write(filestr)
+  h.close()
+  return filename
+
+def applyFileOnRun(fina,runno,experiment=None):
+  execstr = 'bsub -q ' + queue + ' '
+  execstr+= '-o ' + os.path.splitext(fina)[0]
+  if not experiment==None:
+    execstr+= '_'+experiment
+  execstr+= '_run%04d'%runno + '.out '
+  execstr+=' python '
+  execstr+=fina+' '
+  if not experiment==None:
+    execstr+= experiment+' '
+  execstr+= str(runno) + ' '
+  result = subprocess.check_output(execstr, shell=True)
+  #os.system(execstr)
+  return result
+
+def applyOnRunlist(funcs,runnos,experiment,save=True):
+  fina = writePyFile(funcs,experiment=experiment,save=save)
+  returns = []
+  for runno in runnos:
+    returns.append(applyFileOnRun(fina,runno))
+  return returns
+
+
+
+
+
+  
+  
