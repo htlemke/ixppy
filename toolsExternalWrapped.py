8ccf681e8992c65a3a3e9c09b2d5e988825723a4
Henrik Lemke <htlemke@gmail.com>
2014-08-13 09:23:07 -0700

    added better wrapped nansum in toolsExternalWrapped

--- toolsExternalWrapped.py
+++ toolsExternalWrapped.py
@@ -13,3 +13,9 @@ numpyfuncs = [
 
 for funName in numpyfuncs:
   exec('%s = wrapFunc(np.%s)'%(funName,funName))
+
+def Nansum(data):
+  o = np.squeeze(np.apply_over_axes(np.nansum,data,range(1,np.rank(data))))
+  return np.atleast_2d(o).T
+
+nansum = wrapFunc(Nansum)
