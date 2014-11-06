CSPAD functionality and fixes in azimathal averaging (mask logic), some fix in ixppy for empty steps

azimuthalAveraging.py
@@ -517,7 +517,7 @@ class azimuthalBinning:
 
     self.msg("... done")
     self.pol=pol
-    theta_max = np.nanmax(matrix_theta[mask])
+    theta_max = np.nanmax(matrix_theta[~mask])
     self.msg("calculating digitize")
     self.nphi = phiBins
     #if phiBins > 1:
@@ -528,7 +528,7 @@ class azimuthalBinning:
 
     self.idxphi = np.digitize(pbm.ravel(),self.phiVec)-1
     self.matrix_q = 4*np.pi/lam*np.sin(self.matrix_theta/2)
-    q_max = np.nanmax(self.matrix_q[mask])
+    q_max = np.nanmax(self.matrix_q[~mask])
     qbin = np.array(qbin)
     if qbin.size==1:
       self.qbins = np.arange(0,q_max+qbin,qbin)
@@ -539,9 +539,11 @@ class azimuthalBinning:
     self.nq = self.q.size
     self.idxq  = np.digitize(self.matrix_q.ravel(),self.qbins)-1
     last_idx = self.idxq.max()
+    self.idxq[mask.ravel()] = 0; # send the masked ones in the first bin
+
     # 2D binning!
     self.Cake_idxs = np.ravel_multi_index((self.idxphi,self.idxq),(self.nphi,self.nq))
-    self.Cake_idxs[~mask.ravel()] = 0; # send the masked ones in the first bin
+    self.Cake_idxs[mask.ravel()] = 0; # send the masked ones in the first bin
     #print "last index",last_idx
     self.msg("...done")
     self.phi  = np.arange(0,2*np.pi+phibin,phibin)+phibin/2

cspad/cspad.py
@@ -3,7 +3,7 @@ import numpy as np
 import os,sys
 from ixppy import tools,wrapFunc
 import copy
-from ixppy.tools import nfigure,filtvec
+from ixppy.tools import nfigure,filtvec,polygonmask
 #from functools import partial
 
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -413,9 +413,18 @@ class CspadPattern(object):
 def noiseMap(Istack):
   return np.std(np.asfarray(Istack)/sum(Istack),axis=0)
 
-def getNoiseMap(Istack,lims=None):
+def getNoiseMap(Istack,lims_perc=None,lims=None):
   noise = noiseMap(Istack)
   #np.shape(noise)
+  if lims_perc is not None:
+    lims = np.percentile(noise,lims_perc)
+    tools.nfigure('Selected noise limits')
+    pl.clf()
+    tools.histSmart(noise.ravel()[~np.isnan(noise.ravel())],fac=200)
+    pp = plt.axhspan(*lims)
+    plt.gca().add_patch(pp)
+    plt.draw()
+
   if lims==None:
     tools.nfigure('Find noise limits')
     pl.clf()